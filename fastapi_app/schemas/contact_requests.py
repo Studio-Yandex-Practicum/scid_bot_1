a0341b27515046fc0ebe5ab22ad43a356ac19974
Бондаренко В. В. <AK23041989@Live.com>
2024-09-26 12:54:06 +0600

    Добавил модель и схемы, для запросов обратной связи

---
+++ fastapi_app/schemas/contact_requests.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class ContactRequestBase(BaseModel):
+    user_id: int
+    name: str
+    phone: str
+    contact_via_telegram: Optional[bool] = Field(default=False)
+    contact_via_phone: Optional[bool] = Field(default=False)
+
+
+class ContactRequestCreate(ContactRequestBase):
+    pass
+
+
+class ContactRequestUpdate(BaseModel):
+    is_processed: Optional[bool]
+    contact_via_telegram: Optional[bool]
+    contact_via_phone: Optional[bool]
+
+
+class ContactRequestResponse(ContactRequestBase):
+    id: int
+    created_at: datetime = Field(...)
+    closed_at: Optional[datetime] = Field(None)
+
+    class Config:
+        from_attributes = True
