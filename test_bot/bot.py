e8a3870d24a376a28ab01163532129b1e7af0206
Александр Пастух <black_light_@bk.ru>
2024-09-26 17:47:12 +0300

    Лобавил проверку апи

--- test_bot/bot.py
+++ test_bot/bot.py
@@ -1,46 +1,64 @@
 import os
+import aiohttp
+from dotenv import load_dotenv
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram import F
 
-from aiogram import Router
-# from aiogram.utils import Token
-from aiogram import types
+from aiogram import Router, types
 
-# Включаем логирование
+load_dotenv()
+
+
 logging.basicConfig(level=logging.INFO)
 
-# Вставьте сюда свой токен бота
-# TOKEN = os.getenv('TOKEN')
-TOKEN = 
 
-# Создание экземпляра бота
+TOKEN = os.getenv('TOKEN')
+if not TOKEN:
+    raise ValueError("TOKEN environment variable is not set!")
+API_URL = os.getenv('API_URL')
+if not API_URL:
+    raise ValueError("API_URL environment variable is not set!")
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Создание маршрутизатора
-router = Router()
 
-# Хендлер для команды /start
+router = Router()
 
 
 @router.message(Command('start'))
 async def send_welcome(message: Message):
     await message.answer("Проверка связи!!!")
 
-# Хендлер для любого текстового сообщения
+# Проверка API
+@dp.message_handler(Command('api'))
+async def call_api(message: Message):
+    await message.answer("Обращаюсь к FastAPI...")
+
+    # Выполняем запрос к эндпоинту FastAPI
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(f"{API_URL}/api") as response:
+                if response.status == 200:
+                    data = await response.json()  # Парсим JSON-ответ
+                    # Отправляем результат пользователю
+                    await message.answer(f"Ответ от FastAPI: {data}")
+                else:
+                    # Если ответ не OK
+                    await message.answer(f"Ошибка при обращении к FastAPI: статус {response.status}")
+        except Exception as e:
+            # В случае ошибки выводим сообщение об ошибке
+            await message.answer(f"Произошла ошибка при обращении к FastAPI: {str(e)}")
 
 
 @router.message(F.text)
 async def echo(message: Message):
     await message.answer(f"Ты написал: {message.text}")
 
-# Регистрация роутера
 dp.include_router(router)
-
-# Запуск бота
 
 
 async def main():
