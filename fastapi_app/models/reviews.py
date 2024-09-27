7a39ea6365a0196d9a7dfff0c89c804642ffd656
Бондаренко В. В. <AK23041989@Live.com>
2024-09-27 19:28:06 +0600

    Добавил модель для отзывов

---
+++ fastapi_app/models/reviews.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime, Float
+
+from core.db import Base
+
+
+class Review(Base):
+    telegram_user_id = Column(Integer, nullable=False)
+    text = Column(String, nullable=False)
+    rating = Column(Float, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return (
+            f'<Review(id={self.id}, telegram_user_id='
+            f'{self.telegram_user_id}, rating={self.rating}, '
+            f'created_at={self.created_at}'
+        )
