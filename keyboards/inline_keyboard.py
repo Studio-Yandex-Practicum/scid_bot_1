7f00793989b52ad2bc0d2b52e59bcae54b605ce0
Andriy Gorbanenko <an2gor@googlemail.com>
2024-09-30 20:52:49 +0200

    v2

--- keyboards/inline_keyboard.py
+++ keyboards/inline_keyboard.py
@@ -31,7 +31,7 @@ async def inline_menu(buttons: list[dict[str, int, str]],
 
     service_keyboard = InlineKeyboardBuilder()
     service_keyboard.add(InlineKeyboardButton(text="Назад",
-                                           callback_data='2'))
+                                           callback_data="2"))
     service_keyboard.add(InlineKeyboardButton(text="В начало",
                                            callback_data="1"))
     service_keyboard.adjust(1)
