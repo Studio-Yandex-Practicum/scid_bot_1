c7a50b9afe4d384360e4203e4b4df05992c34eac
Andriy Gorbanenko <an2gor@googlemail.com>
2024-10-08 08:56:50 +0200

    Inline Keyboard с одной кнопкой, добавлен type hint, измененa docstring

--- bot/keyboards/inline_keyboard.py
+++ bot/keyboards/inline_keyboard.py
@@ -2,44 +2,38 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-async def inline_menu(buttons, columns=2, start_id=1) -> InlineKeyboardMarkup:
+async def inline_menu(button: dict, start_id: int) -> InlineKeyboardMarkup:
     """
-    buttons = [
-    {
-        "id": 3,
-        "label": "test_1",
-        "parent_id": 2
-    },
-    {
-        "id": 4,
-        "label": "test 2",
-        "parent_id": 2
-    }
-    ]
-
-    columns = Buttons in row
+    button example:
+    button =
+        {
+            "id": 3,
+            "label": "test_1",
+            "parent_id": 2
+        }
+    callback_data examples:
 
     callback_data = "button_id:int,parent_id:int"
-    Examples:
+        for normal button
         callback_data = "3,2"
-        callback_data = "2" # Back
+
+        for "Back" button
+        callback_data = "2"
     """
 
     keyboard = InlineKeyboardBuilder()
 
-    for button in buttons:
-        keyboard.add(InlineKeyboardButton(text=button["label"],
-                                          callback_data=(
-                                              f"{button['id']},"
-                                              f"{button['parent_id']}")))
-    keyboard.adjust(columns)
-# Service keyboard ("Back" and "to start").
-# One Button at row
-    parent_id = buttons[0].get('parent_id')
+    keyboard.add(InlineKeyboardButton(text=button["label"],
+                                      callback_data=(
+                                          f"{button['id']},"
+                                          f"{button['parent_id']}")))
+   # Service keyboard ("Back" and "to start").
+    # One Button at row
+
     service_keyboard = InlineKeyboardBuilder()
     service_keyboard.add(InlineKeyboardButton(text="Назад",
                                               callback_data=(
-                                                  f"{parent_id}")))
+                                                  f"{button['parent_id']}")))
     service_keyboard.add(InlineKeyboardButton(text="В начало",
                                               callback_data=(
                                                   f"{start_id}")))
