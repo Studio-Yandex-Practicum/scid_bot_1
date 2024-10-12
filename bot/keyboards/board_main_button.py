8f56c9fe9434394798d6abe4b93fdfe6f7f9570b
Andrey <shooroop87@mail.ru>
2024-10-12 14:26:07 +0300

    откатил изменение
    откатил изменение

--- bot/keyboards/board_main_button.py
+++ bot/keyboards/board_main_button.py
@@ -1,18 +1,7 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def make_main_button_keyboard(items: list[dict]) -> InlineKeyboardMarkup:
-    """
-    items: список словарей, где каждый элемент содержит 'text' и 'url'
-    Пример: [{"text": "Google", "url": "https://google.com"}, {"text": "Yandex", "url": "https://yandex.ru"}]
-    """
-    # Создаем клавиатуру
-    keyboard = InlineKeyboardMarkup()
 
-    # Проходим по каждому элементу из списка
-    for item in items:
-        # Создаем кнопку с текстом и url
-        button = InlineKeyboardButton(text=item['text'], url=item['url'])
-        # Добавляем кнопку в клавиатуру
-        keyboard.add(button)
+def make_main_button_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
 
-    return keyboard
+    row = [KeyboardButton(text=item) for item in items]
+    return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
