944fad9a8b8d15349b8549e0c7f27c7223f34121
Andrey <shooroop87@mail.ru>
2024-10-12 14:19:01 +0300

    исправил items
    обновил make_main_button_keyboard, чтобы можно было передать в items и url

--- bot/keyboards/board_main_button.py
+++ bot/keyboards/board_main_button.py
@@ -1,7 +1,18 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def make_main_button_keyboard(items: list[dict]) -> InlineKeyboardMarkup:
+    """
+    items: список словарей, где каждый элемент содержит 'text' и 'url'
+    Пример: [{"text": "Google", "url": "https://google.com"}, {"text": "Yandex", "url": "https://yandex.ru"}]
+    """
+    # Создаем клавиатуру
+    keyboard = InlineKeyboardMarkup()
 
-def make_main_button_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
- 
-    row = [KeyboardButton(text=item) for item in items]
-    return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
+    # Проходим по каждому элементу из списка
+    for item in items:
+        # Создаем кнопку с текстом и url
+        button = InlineKeyboardButton(text=item['text'], url=item['url'])
+        # Добавляем кнопку в клавиатуру
+        keyboard.add(button)
+
+    return keyboard
