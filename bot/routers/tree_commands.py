21772d98b318e4b0ae1f4fa443d09ec313126ddc
Бондаренко В. В. <AK23041989@Live.com>
2024-10-16 20:54:19 +0700

    Исправил отображение id в дереве

--- bot/routers/tree_commands.py
+++ bot/routers/tree_commands.py
@@ -2,6 +2,7 @@ from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
+from api.api_service import get_main_menu_button
 from utils.tree_utils import format_text, render_recursive
 
 
@@ -22,9 +23,13 @@ async def cmd_tree(message: Message):
 @router.message(F.text == "Посмотреть структуру")
 async def send_tree(message: Message):
     # Тут передаем id первой кнопки, лучше получать ее из API
-    tree_data = await render_recursive(1)
+    main_menu_button = await get_main_menu_button()
+    tree_data = await render_recursive(main_menu_button["id"])
     if tree_data:
         result = format_text(tree_data)
-        await message.answer(f"Главное меню\n{result}", parse_mode="HTML")
+        await message.answer(
+            f"Главное меню ({main_menu_button['id']})\n{result}",
+            parse_mode="HTML"
+        )
     else:
         await message.answer("Не удалось получить данные от API.")
